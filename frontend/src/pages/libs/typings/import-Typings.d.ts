c3938697e517d003bad63c7bfcc543ce193becb3
Rafsun Jany Arman <armanrafsunjany@gmail.com>
2020-12-14 02:17:23 +0600

    add: custom typings for ts

---
+++ frontend/src/pages/libs/typings/import-Typings.d.ts
@@ -0,0 +1,14 @@
+declare module '*.png' {
+  const value: any;
+  export default value;
+}
+
+declare module '*.mp4' {
+  const value: any;
+  export default value;
+}
+
+declare module '*.svg' {
+  const value: any;
+  export default value;
+}
